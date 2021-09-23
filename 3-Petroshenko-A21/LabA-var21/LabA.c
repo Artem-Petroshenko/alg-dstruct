8a6a25ef576eb69ca3b628b121a61e875f1351ef
Artem-Petroshenko <petroshenko.av@edu.spbstu.ru>
2021-09-23 14:54:16 +0300

    Now actually fixed add and sort func

--- 3-Petroshenko-A21/LabA-var21/LabA.c
+++ 3-Petroshenko-A21/LabA-var21/LabA.c
@@ -8,14 +8,14 @@ List_t* ListCreate(const char* filename) {								//creating the list with the f
 	int Key;
 
 	fscanf(F, "%s %i", Word, &Key);
-	node_t* head = malloc(sizeof(node_t));
+	Node_t* head = (Node_t*)malloc(sizeof(Node_t));
 	if (!head)															//checking for right memory allocation
 		return 0;
 	head->data.Key = Key;
 	head->data.Word = (char*)malloc(sizeof(char) * N);
 	strcpy(head->data.Word, Word);
 	head->next = NULL;
-	List_t* List = malloc(sizeof(List_t));
+	List_t* List = (List_t*)malloc(sizeof(List_t));
 	if (!List)
 		return 0;
 	List->head = head;
@@ -25,11 +25,12 @@ List_t* ListCreate(const char* filename) {								//creating the list with the f
 }
 
 void ListDestroy(List_t* List) {										//freeing the memory
-	node_t* temp_pointer = List->head;
+	Node_t* temp_pointer = List->head;
 	while (temp_pointer)
 	{
-		node_t* deleted = temp_pointer;
+		Node_t* deleted = temp_pointer;
 		temp_pointer = temp_pointer->next;
+		free(deleted->data.Word);
 		free(deleted);
 	}
 	free(List);
@@ -38,72 +39,74 @@ void ListDestroy(List_t* List) {										//freeing the memory
 void Fill(const char* filename, List_t* List) {							//just reading the elements from file
 	char Word[N];														
 	int Key;
-	FILE* F = fopen(filename, "r");
-	if (F == NULL)
-		return;
+	FILE* F;
+	if ((F = fopen(filename, "r")) == NULL)								//checking for right opening the file
+		return 0;
+
 	fscanf(F, "%s %i", Word, &Key);
 	while (fscanf(F, "%s %i", Word, &Key) != EOF)
 	{
-		node_t* new_element = malloc(sizeof(node_t));
+		Node_t* new_element = (Node_t*)malloc(sizeof(Node_t));
 		if (!new_element)
 			return;
 		new_element->data.Word = (char*)malloc(sizeof(char) * N);
+		if (!new_element->data.Word)
+			return;
 		strcpy(new_element->data.Word, Word);
 		new_element->data.Key = Key;
-		Add_and_Sort(List, new_element);
+		Add(List, new_element);
 	}
 
 	fclose(F);
 	return;
 }
 
-void Add_and_Sort(List_t* List, node_t* new_element) {					//adding the new element in the right place in the list
-	node_t* temp_pointer = List->head;										
-	while (temp_pointer->next)
+Node_t* Search(List_t* List, int DataKey) {
+	Node_t* pointer = List->head;
+	if (pointer->data.Key > DataKey)
+		return NULL;
+	while (pointer->next && pointer->next->data.Key < DataKey)
 	{
-		if (temp_pointer->data.Key == new_element->data.Key)			//checking error cases
-		{
-			puts("ERROR: The key numbers are equal!\n");
-			return;
-		}
-		else if (temp_pointer->data.Key < new_element->data.Key)		//going through the list
-			temp_pointer = temp_pointer->next;
-		else if (temp_pointer->data.Key > new_element->data.Key)
-		{
-			if (temp_pointer == List->head)								//in case of finding the smallest current key-number 
-			{
-				new_element->next = temp_pointer;
-				List->head = new_element;
-				return;
-			}
-			else
-			{
-				new_element->next = temp_pointer->next;
-				temp_pointer->next = new_element;
-				data_t temp_data = temp_pointer->data;
-				temp_pointer->data = new_element->data;
-				new_element->data = temp_data;
-				return;
-			}
-		}
+		pointer = pointer->next;
 	}
-	new_element->next = temp_pointer->next;						//comparing with the last element
-	temp_pointer->next = new_element;							//it's more comfortable because of
-	if (temp_pointer->data.Key > new_element->data.Key)			//NULL pointering
+	return pointer;
+}
+
+int Add(List_t* List, Node_t* new_element) {
+	Node_t* place = Search(List, new_element->data.Key);
+	if (!place)
 	{
-		data_t temp_data = temp_pointer->data;
-		temp_pointer->data = new_element->data;
-		new_element->data = temp_data;
+		if (new_element->data.Key == List->head->data.Key)
+			return Error;
+		new_element->next = List->head;
+		List->head = new_element;
+		return Add_to_begining;
 	}
-	return;
+	else
+	{
+		if (place->data.Key == new_element->data.Key)
+			return Error;
+		if (place->next != NULL && place->next->data.Key == new_element->data.Key)
+			return Error;
+		new_element->next = place->next;
+		place->next = new_element;
+		return Add_not_to_begining;
+	}
 }
 
 void Output(List_t* List) {
-	node_t* temp_pointer = List->head;
+	Node_t* temp_pointer = List->head;
 	while (temp_pointer)
 	{
 		printf("%s %i\n", temp_pointer->data.Word, temp_pointer->data.Key);
 		temp_pointer = temp_pointer->next;
 	}
+	free(temp_pointer);
 	return;
+}
+
+void MemoryLeaks(void) {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
 }
--- 3-Petroshenko-A21/LabA-var21/LabA.h
+++ 3-Petroshenko-A21/LabA-var21/LabA.h
@@ -1,27 +1,49 @@
 #pragma once
+#ifndef _MY_H
+#define _MY_H
+
 #pragma warning(disable:4996)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <crtdbg.h>
+#define _CRTDBG_MAP_ALLOC
+#define _CRT_SECURE_NO_WARNINGS
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+ 
 #define N 50
+#define Error -1
+#define Add_to_begining 1
+#define Add_not_to_begining 2
 
 typedef struct {
 	char* Word;
 	int Key;
 }data_t;
 
-typedef struct node_t {
+typedef struct Node_t {
 	data_t data;
-	struct node_t* next;
-}node_t;
+	struct Node_t* next;
+}Node_t;
 
 typedef struct {
-	node_t* head;
+	Node_t* head;
 }List_t;
 
 List_t* ListCreate(const char* filename);
 void ListDestroy(List_t* List);
 void Fill(const char* filename, List_t* List);
-void Add_and_Sort(List_t* List, node_t* new_element);
+Node_t* Search(List_t* List, int DataKey);
+int Add(List_t* List, Node_t* new_element);
 void Output(List_t* List);
+void MemoryLeaks(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
