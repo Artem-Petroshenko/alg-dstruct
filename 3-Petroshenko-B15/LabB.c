dec9a4363083c7dbe37d47a74e82c3801120fe93
Artem-Petroshenko <petroshenko.av@edu.spbstu.ru>
2021-10-22 16:14:14 +0300

    Initial commit (19/100)
    written all functions
    19/100 on testing system

---
+++ 3-Petroshenko-B15/LabB.c
@@ -0,0 +1,198 @@
+#include "LabB.h"
+
+//pointer on the begining of the using heap
+void* sMemory = NULL;
+//size of the heap
+int sizeofs = 0;
+//pointer on list of empty blocks
+void* sHead = NULL;
+
+//next 4 funcs is extra for control data in memory blocks
+int* GetSizePtr(void* descriptor) {
+	return (int*)descriptor;
+}
+
+void** GetNextPtr(void* descriptor) {
+	return (void**)((char*)descriptor + sizeof(int));
+}
+
+void** GetPrevPtr(void* descriptor) {
+	return (void**)((char*)descriptor + sizeof(int) + sizeof(void*));
+}
+
+//Flag is contained in the end of the block
+int* GetFlagPtr(void* descriptor) {
+	return (int*)((char*)descriptor + *GetSizePtr(descriptor) - sizeof(int));
+}
+
+int MemInit(void* pMemory, int size) {
+	if (!pMemory)
+		return Error;
+	if (size <= MemGetMinimumSize())
+		return Error;
+	*GetSizePtr(pMemory) = size;
+	*GetNextPtr(pMemory) = NULL;
+	*GetPrevPtr(pMemory) = NULL;
+	*GetFlagPtr(pMemory) = -size;
+	sMemory = pMemory;
+	sizeofs = size;
+	sHead = pMemory;
+	return Ok;
+}
+
+void MemDone() {
+	void* iterator = sMemory;
+	while ((char*)iterator < (char*)sMemory + sizeofs)
+	{
+		//Positive value of flag means that this block is used by user
+		if (*GetFlagPtr(iterator) > 0)
+			fprintf(stderr, "MEMORY LEAK at 0x%p\n", (char*)iterator + sizeof(int) + sizeof(void*) + sizeof(void*));
+		iterator = (void*)((char*)iterator + *GetSizePtr(iterator));
+	}
+	return;
+}
+
+void* MemAlloc(int size) {
+	if (size > sizeofs - MemGetBlockSize())
+		return NULL;
+	void* iterator = sHead;
+	void* fitblock = NULL;
+	int min = *GetSizePtr(iterator);
+	//searching for best fit block
+	while (iterator != NULL)
+	{
+		if (*GetSizePtr(iterator) > size && *GetSizePtr(iterator) < min)
+		{
+			fitblock = iterator;
+			min = *GetSizePtr(iterator);
+		}
+		iterator = GetNextPtr(iterator);
+	}
+	if (!fitblock)
+		return NULL;
+	//fragmentation:
+	//if we have enough memory to create new descriptor for empty part
+	if (*GetSizePtr(fitblock) > MemGetBlockSize() + size + MemGetBlockSize())
+	{
+		//right part of the block, i.e. empty part
+		void* newdescriptor = (void*)((char*)fitblock + MemGetBlockSize() + size);
+		*GetSizePtr(newdescriptor) = *GetSizePtr(fitblock) - size - MemGetBlockSize();
+		*GetNextPtr(newdescriptor) = *GetNextPtr(fitblock);
+		*GetPrevPtr(newdescriptor) = *GetPrevPtr(fitblock);
+		*GetFlagPtr(newdescriptor) = -*GetSizePtr(newdescriptor);
+		//if first block is the best fit
+		if (!*GetPrevPtr(fitblock))
+			sHead = newdescriptor;
+		else
+			*GetNextPtr(*GetPrevPtr(fitblock)) = newdescriptor;
+	}
+	else
+	{
+		//if first block is the best fit
+		
+	}
+	//left part of the block, i.e. user's part
+	*GetNextPtr(fitblock) = NULL;
+	*GetPrevPtr(fitblock) = NULL;
+	*GetSizePtr(fitblock) = MemGetBlockSize() + size;
+	*GetFlagPtr(fitblock) = *GetSizePtr(fitblock);
+	//returning pointer exactly on data
+	return (void*)((char*)fitblock + sizeof(int) + sizeof(void*) + sizeof(void*));
+}
+
+void MemFree(void* p) {
+	void* pdescriptor = (void*)((char*)p - sizeof(int) - sizeof(void*) - sizeof(void*));
+	void* leftblock = NULL;
+	void* rightblock = NULL;
+	//if we are going to free the first block in heap
+	if ((char*)pdescriptor - 1 > (char*)sMemory)
+		leftblock = (void*)((char*)pdescriptor - abs(*((char*)pdescriptor - sizeof(int))));
+	//if we are going to free the last block in heap
+	if ((char*)pdescriptor + *GetSizePtr(pdescriptor) < (char*)sMemory + sizeofs)
+		rightblock = (void*)((char*)pdescriptor + *GetSizePtr(pdescriptor));
+	Merge right = NoMerge, left = NoMerge;
+	if (leftblock && *GetFlagPtr(leftblock) < 0)
+		left = MergeLeft;
+	if (rightblock && *GetFlagPtr(rightblock) < 0)
+		right = MergeRight;
+	//Start merging
+	if (right == MergeRight && left == MergeLeft)
+	{
+		//if right block is the first
+		if (!*GetPrevPtr(rightblock))
+		{
+			*GetNextPtr(*GetPrevPtr(leftblock)) = *GetNextPtr(leftblock);
+			//if left block is the last pointer "next" is NULL
+			if (*GetNextPtr(leftblock))
+				*GetPrevPtr(*GetNextPtr(leftblock)) = *GetPrevPtr(leftblock);
+			*GetNextPtr(leftblock) = *GetNextPtr(rightblock);
+			*GetPrevPtr(leftblock) = NULL;
+			sHead = leftblock;
+		}
+		//if left block is the first
+		if (!*GetPrevPtr(leftblock))
+		{
+			*GetNextPtr(*GetPrevPtr(rightblock)) = *GetNextPtr(rightblock);
+			//if right block is the last pointer "next" is NULL
+			if(*GetNextPtr(rightblock))
+				*GetPrevPtr(*GetNextPtr(rightblock)) = *GetPrevPtr(rightblock);
+		}
+		//if left and right blocks are not the first
+		if (*GetPrevPtr(leftblock) && *GetPrevPtr(rightblock))
+		{
+			*GetNextPtr(*GetPrevPtr(rightblock)) = *GetNextPtr(rightblock);
+			if (*GetNextPtr(rightblock))
+				*GetPrevPtr(*GetNextPtr(rightblock)) = *GetPrevPtr(rightblock);
+			*GetSizePtr(leftblock) += *GetSizePtr(pdescriptor) + *GetSizePtr(rightblock);
+			*GetFlagPtr(leftblock) = -*GetSizePtr(leftblock);
+		}
+	}
+	if (left == MergeLeft)
+	{
+		//it doen't matter if left block is the first/last or not
+		*GetSizePtr(leftblock) += *GetSizePtr(pdescriptor);
+		*GetFlagPtr(leftblock) = -*GetSizePtr(leftblock);
+	}
+	if (right == MergeRight)
+	{
+		*GetNextPtr(pdescriptor) = *GetNextPtr(rightblock);
+		*GetPrevPtr(pdescriptor) = *GetPrevPtr(rightblock);
+		//if right block is the first
+		if (!*GetPrevPtr(rightblock))
+		{
+			//if right block is the only one empry block
+			if (*GetNextPtr(rightblock))
+				*GetPrevPtr(*GetNextPtr(pdescriptor)) = pdescriptor;
+			sHead = pdescriptor;
+		}
+		//if right block is not the first
+		else
+		{
+			//if the right block is the last
+			if (*GetNextPtr(rightblock))
+				*GetPrevPtr(*GetNextPtr(pdescriptor)) = pdescriptor;
+			*GetNextPtr(*GetPrevPtr(pdescriptor)) = pdescriptor;
+		}
+	}
+	if (left == NoMerge && right == NoMerge)
+	{
+		//Adding freeing memory to the begining of the list of empty blocks
+		*GetNextPtr(pdescriptor) = sHead;
+		*GetPrevPtr(pdescriptor) = *GetPrevPtr(sHead);
+		*GetPrevPtr(sHead) = pdescriptor;
+		sHead = pdescriptor;
+		*GetFlagPtr(pdescriptor) *= -1;
+	}
+}
+
+int MemGetMinimumSize() {
+	return sizeof(int) + sizeof(void*) + sizeof(void*) + sizeof(int);
+}
+
+int MemGetBlockSize() {
+	return sizeof(int) + sizeof(void*) + sizeof(void*) + sizeof(int);
+}
+
+int main() {
+	return 0;
+}
--- 3-Petroshenko-B15/LabB.h
+++ 3-Petroshenko-B15/LabB.h
@@ -1,22 +1,39 @@
-#ifndef __MEMALLOCATOR_H__E71A34CB
-#define __MEMALLOCATOR_H__E71A34CB
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <math.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef enum {
+	NoMerge,
+	MergeRight,
+	MergeLeft
+}Merge;
+
+#define Ok 1
+#define Error 0
+
+int* GetSizePtr(void* descriptor);
+void** GetNextPtr(void* descriptor);
+void** GetPrevPtr(void* descriptor);
+int* GetFlagPtr(void* descriptor);
+
 // Init memory system with memory block pMemory.
-int meminit(void *pMemory, int size);
+int MemInit(void* pMemory, int size);
 
 // You can implement memory leak checks here
-void memdone();
+void MemDone();
 
 // Allocate memory block of size 'size'.
 // Returns pointer to memory block is success, 0 otherwise
-void *memalloc(int size);
+void* MemAlloc(int size);
 
 // Free memory previously allocated by memalloc
-void memfree(void *p);
+void MemFree(void* p);
 
 // Return minimum size in bytes of the memory pool to allocate 0-bytes block
 // use case:
@@ -27,7 +44,7 @@ void memfree(void *p);
 // memfree(p);
 // memdone();
 // free(ptr);
-int memgetminimumsize();
+int MemGetMinimumSize();
 
 
 // Returns size in bytes of additional information per allocation
@@ -42,11 +59,9 @@ int memgetminimumsize();
 // memfree(p1);
 // memdone();
 // free(ptr);
-int memgetblocksize();
+int MemGetBlockSize();
 
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
